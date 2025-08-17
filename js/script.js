7feec48bcb4528fb35100e1f6c3d4cb826fd48f1
Ilya Pridanov <ik.front2558988@gmail.com>
2025-08-17 20:23:30 +0300

    feat:slider-mobile

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-document.addEventListener('DOMContentLoaded', function() {
-  function getRunningLineClone () {
+document.addEventListener('DOMContentLoaded', function () {
+  function getRunningLineClone() {
     const runningLineContent = document.querySelector('.running-line__content');
 
     const texts = document.querySelectorAll('.running-line__text');
@@ -9,5 +9,58 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   };
 
+  function getSlider() {
+    let blockSliderInstance = null;
+    const sliderContainers = document.querySelector('.stages');
+
+    function getBlockSlider(slider) {
+      if (slider) {
+        const container = slider.querySelector('.swiper-container');
+        const slides = slider.querySelectorAll('.swiper-slide');
+        const prev = slider.querySelector('.js-btn-next');
+        const next = slider.querySelector('.js-btn-prev');
+        const pagination = slider.querySelector('.swiper-pagination');
+        const loop = false;
+        const direction = 'horizontal';
+        const autoHeight = false;
+
+        if (slides.length >= 2) {
+          return new window.Swiper(container, {
+            direction: direction,
+            loop: loop,
+            autoHeight: autoHeight,
+            slidesPerView: 1,
+            spaceBetween: 20,
+            navigation: {
+              nextEl: next,
+              prevEl: prev,
+            },
+            pagination: {
+              el: pagination,
+              clickable: true,
+            },
+          });
+        }
+      }
+    }
+
+    function handleSliderForMobile() {
+      if (window.innerWidth <= 576) {
+        if (!blockSliderInstance) {
+          blockSliderInstance = getBlockSlider(sliderContainers);
+        }
+      } else {
+        if (blockSliderInstance && typeof blockSliderInstance.destroy === 'function') {
+          blockSliderInstance.destroy(true, true);
+          blockSliderInstance = null;
+        }
+      }
+    }
+
+    handleSliderForMobile();
+    window.addEventListener('resize', handleSliderForMobile);
+  }
+
   getRunningLineClone();
+  getSlider();
 });
